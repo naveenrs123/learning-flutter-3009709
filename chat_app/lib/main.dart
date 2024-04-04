3ff74bf22f933b5ba2ae15e74313f2a061655b21
naveenrs123 <naveen.sivasankar1234@gmail.com>
2024-04-03 18:15:56 -0700

    Complete exercise

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,6 +7,8 @@ void main() {
 }
 
 class ChatApp extends StatelessWidget {
+  const ChatApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
